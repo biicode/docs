58ec57a5015ec9b148ad87e7000839edf5b175d4
Luis Pedraza <luispedraza@gmail.com>
2013-10-30 13:48:28 +0100

    compiled bootstrap

--- conf.py
+++ conf.py
@@ -103,11 +103,10 @@ pygments_style = 'colorful'
 # html_theme = 'agogo'
 
 # bootstrap theme:
-# html_theme = 'bootstrap'
-# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'bootstrap'
 
 # sphinx rtd theme:
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
