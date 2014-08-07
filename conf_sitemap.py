2c9e670054394b7950fc4f00a5efc5ad5cdd6766
Luis Martínez de Bartolomé Izquierdo <lmartinez@biicode.com>
2014-08-07 13:45:48 +0200

    Trace for sitemap genration

--- conf_sitemap.py
+++ conf_sitemap.py
@@ -1,6 +1,6 @@
 import sys, os, shutil, getopt
 
-print "argvs: %s" % str(sys.argv)
+print "argvs: %s" % str(sys.argv[2:])
 opts, args = getopt.getopt( sys.argv[1:], 'ab:d:c:CD:A:NEqQP:w')
 print 'opts: ', opts
 print 'args: ', args
