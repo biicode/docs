12968bcd265b63dd0abc9632393e87277c48fa70
Julia S.Simon <julia.simon@biicode.com>
2014-09-24 10:12:59 +0200

    corrected example

--- _static/code/cpp/examples/ptypes/stream.cpp
+++ _static/code/cpp/examples/ptypes/stream.cpp
@@ -7,12 +7,12 @@
 using namespace pt;
 
 int main() {
-        const char* fname = "stmtest.txt\n";
+        const char* fname = "stmtest.txt";
         outfile f(fname,false);
         f.set_bufsize(1024);  // the default value in this version is 8192
         try {
                 f.open();
-                f.put("This is a test file.");
+                f.put("This is a test file.\n");
                 f.close();
         } catch (estream* e) {
                 perr.putf(e->get_message());
