bb50121ae490cc1f38f593c0e56d35effe5b9b24
danimtb <danimanzaneque@gmail.com>
2015-03-02 11:45:36 +0100

    opencv example and new examples order

---
+++ _static/code/cpp/examples/opencv/main.cpp
@@ -0,0 +1,16 @@
+#include "diego/opencv/opencv/cv.h"      // include it to used Main OpenCV functions.
+#include "diego/opencv/opencv/highgui.h" //include it to use GUI functions.
+ 
+int main(int argc, char** argv)
+{
+    //bii://bii.png
+    IplImage* img = cvLoadImage( "examples/opencv_sample/bii.png" );
+    cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
+
+    cvShowImage("Example1", img);
+    cvMoveWindow("Example1", 0, 0);
+    cvWaitKey(0);
+    cvReleaseImage( &img );
+    cvDestroyWindow( "Example1" );
+    return 0;
+}
